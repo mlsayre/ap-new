98b716cada53f9ef5186b36f668b223585d76a2d
Matt Sayre <mlsayre@gmail.com>
2015-12-14 23:55:33 -0800

    first commit

---
+++ ap-new.jsx
@@ -0,0 +1,10 @@
+Rooms = new Mongo.Collection("rooms");
+
+if (Meteor.isClient) {
+  // This code is executed on the client only
+
+  Meteor.startup(function () {
+    // Use Meteor.startup to render the component after the page is ready
+    ReactDOM.render(<App />, document.getElementById("render-target"));
+  });
+}
